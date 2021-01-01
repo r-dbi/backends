5a5e7885a2b25d9378ec73a3ce0e2b863758f119
Kirill Müller <krlmlr+r@mailbox.org>
2021-01-01 08:48:31 +0100

    Tweak interface

--- R/json.R
+++ R/json.R
@@ -11,13 +11,15 @@ pkg_tbl_to_json <- function(pkg_tbl) {
 read_by_pkg <- function() {
   files <- dir("docs/by-package", full.names = TRUE)
   names <- sub("[.][^.]*$", "", basename(files))
-  map(set_names(files, names), jsonlite::read_json)
+  text <- map(set_names(files, names), readLines)
+
+  tibble(path = files, name = names, old = text)
 }
 
 create_all_json <- function() {
   all <- read_by_pkg()
 
-  all_named <- unname(imap(all, ~ { rlang::list2(name = .y, !!!.x) }))
+  all_named <- unname(map2(all$old, all$name, ~ { rlang::list2(name = .y, !!!jsonlite::fromJSON(.x)) }))
 
   all_url <- map(all_named, ~ {
     if (!is.null(.x$url)) .x$url <- I(.x$url)
