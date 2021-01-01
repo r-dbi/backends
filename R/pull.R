576f613eba8624364350342833683283a95acf3c
Kirill Müller <krlmlr+r@mailbox.org>
2021-01-01 14:50:09 +0100

    Always check out explicitly

--- R/pull.R
+++ R/pull.R
@@ -6,23 +6,31 @@ pr_new <- function(path, new) withAutoprint({
   exists <- gert::git_branch_exists(name, local = FALSE)
   exists
 
-  if (exists) {
-    gert::git_branch_checkout(name)
+  if (!exists) {
   } else {
+    # checkout = TRUE didn't work
+    message("Creating new branch: ", name)
     gert::git_branch_create(name)
   }
+  message("Checking out existing branch:", name)
+  gert::git_branch_checkout(name)
   on.exit(gert::git_branch_checkout(old_branch))
 
+  message("Writing ", path)
   writeLines(new, path)
   create_all_json()
 
   if (nrow(gert::git_status()) > 0) {
+    message("Committing")
     title <- paste0("New package: ", name)
     body <- "Merge this if you think this is a DBI backend."
 
     gert::git_add("docs")
     gert::git_commit(title)
+
+    message("Pushing")
     gert::git_push(set_upstream = TRUE)
+
     # FIXME: Hard code
     gh::gh(
       "/repos/r-dbi/backends/pulls", head = name, base = old_branch,
