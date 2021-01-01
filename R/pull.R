b6ac6fd3da7f302d3eb47308981e3e1177188cc0
Kirill Müller <krlmlr+r@mailbox.org>
2021-01-01 17:24:08 +0100

    Search criteria

--- R/pull.R
+++ R/pull.R
@@ -30,7 +30,7 @@ pr_new <- function(path, new) {
   title <- paste0("New package: ", name)
   body <- paste0(
     "Merge this if you think this is a DBI backend. Convert to a draft and leave open if not.\n\n",
-    "Decision based on: https://github.com/cran/",  name, "/search?q=DBIConnection+setMethod"
+    "Decision based on: https://github.com/cran/",  name, "/search?q=DBIConnection+setClass"
   )
 
   pr_send(path, old_branch, title, body)
@@ -47,7 +47,7 @@ pr_old <- function(path) {
   title <- paste0("Removed package: ", name)
   body <- paste0(
     "Merge this if you no longer think this is a DBI backend. Convert to a draft and leave open if it is a DBI backend.\n\n",
-    "Decision based on: https://github.com/cran/",  name, "/search?q=DBIConnection+setMethod"
+    "Decision based on: https://github.com/cran/",  name, "/search?q=DBIConnection+setClass"
   )
 
   pr_send(path, old_branch, title, body)
