0a18965ef245c64918285b4e70e051115f036006
Kirill Müller <krlmlr+r@mailbox.org>
2021-01-01 10:20:45 +0100

    Avoid cleaning

--- script/prepare.R
+++ script/prepare.R
@@ -4,10 +4,7 @@ pkgload::load_all()
 
 pkg_tbl <- fetch_pkg_tbl()
 
-dir.create("docs", showWarnings = FALSE)
-
-unlink("docs/by-package", recursive = TRUE)
-dir.create("docs/by-package", showWarnings = FALSE)
+dir.create("docs/by-package", showWarnings = FALSE, recursive = TRUE)
 
 new <- pkg_tbl_to_json(pkg_tbl)
 
